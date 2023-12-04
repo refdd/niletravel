311455e6abf37d9a3fcf9e665401eb2a00570c75
mohamed refat <92251671+refdd@users.noreply.github.com>
2023-12-04 02:59:25 +0200

    test 4

--- src/app/page.js
+++ src/app/page.js
@@ -3,31 +3,35 @@ import SearchSection from "@/components/search/SearchSection";
 import React, { Suspense } from "react";
 import dynamic from "next/dynamic";
 import RowsPackage from "@/components/tours/RowsPackage";
+import RowsOffers from "@/components/offers/RowsOffers";
+import FindPerfectTour from "@/components/perfectTour/FindPerfectTour";
+import Testimonials from "@/components/testimonials/Testimonials";
+import RowBlog from "@/components/blog/RowBlog";
 const ExploreSection = dynamic(() =>
   import("@/components/explore/ExploreSection")
 );
 const FaqSection = dynamic(() => import("@/components/FaqSection/FaqSection"));
 const OurPartners = dynamic(() => import("@/components/partners/OurPartners"));
-const RowBlog = dynamic(() => import("@/components/blog/RowBlog"));
-// const RowsPackage = dynamic(() => import("@/components/tours/RowsPackage"));
-const RowsOffers = dynamic(() => import("@/components/offers/RowsOffers"));
-const Testimonials = dynamic(() =>
-  import("@/components/testimonials/Testimonials")
-);
-const FindPerfectTour = dynamic(() =>
-  import("@/components/perfectTour/FindPerfectTour")
-);
+// const RowBlog = dynamic(() => import("@/components/blog/RowBlog"));
+// // const RowsPackage = dynamic(() => import("@/components/tours/RowsPackage"));
+// const RowsOffers = dynamic(() => import("@/components/offers/RowsOffers"));
+// const Testimonials = dynamic(() =>
+//   import("@/components/testimonials/Testimonials")
+// );
+// const FindPerfectTour = dynamic(() =>
+//   import("@/components/perfectTour/FindPerfectTour")
+// );
 function HomePage() {
   return (
     <div>
       <MainHeader />
       {/* <SearchSection /> */}
       <RowsPackage />
+      <RowsOffers />
+      <FindPerfectTour />
+      <Testimonials />
+      <RowBlog />
       <Suspense fallback={<>...</>}>
-        <RowsOffers />
-        <FindPerfectTour />
-        <Testimonials />
-        <RowBlog />
         <ExploreSection />
         <OurPartners />
         <FaqSection />
