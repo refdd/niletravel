cd752ef09431bfd356e4427c0e76dff62bdae852
mohamed refat <92251671+refdd@users.noreply.github.com>
2023-12-04 02:51:51 +0200

    test 2

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,14 @@ import MainHeader from "@/components/headers/MainHeader";
 import SearchSection from "@/components/search/SearchSection";
 import React, { Suspense } from "react";
 import dynamic from "next/dynamic";
+import RowsPackage from "@/components/tours/RowsPackage";
 const ExploreSection = dynamic(() =>
   import("@/components/explore/ExploreSection")
 );
 const FaqSection = dynamic(() => import("@/components/FaqSection/FaqSection"));
 const OurPartners = dynamic(() => import("@/components/partners/OurPartners"));
 const RowBlog = dynamic(() => import("@/components/blog/RowBlog"));
-const RowsPackage = dynamic(() => import("@/components/tours/RowsPackage"));
+// const RowsPackage = dynamic(() => import("@/components/tours/RowsPackage"));
 const RowsOffers = dynamic(() => import("@/components/offers/RowsOffers"));
 const Testimonials = dynamic(() =>
   import("@/components/testimonials/Testimonials")
@@ -21,8 +22,8 @@ function HomePage() {
     <div>
       <MainHeader />
       <SearchSection />
+      <RowsPackage />
       <Suspense fallback={<>...</>}>
-        <RowsPackage />
         <RowsOffers />
         <FindPerfectTour />
         <Testimonials />
