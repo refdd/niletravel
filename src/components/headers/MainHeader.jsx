47765e06ba0e514d805778d28f16c5885b8222a1
mohamed refat <92251671+refdd@users.noreply.github.com>
2023-12-09 03:37:51 +0200

    psh f

--- src/components/headers/MainHeader.jsx
+++ src/components/headers/MainHeader.jsx
@@ -1,31 +1,17 @@
 import React from "react";
-import imgSrc from "../../../public/assets/images/bghome.webp";
-import Image from "next/image";
 async function MainHeader() {
   return (
     <div className="">
-      <div className="relative  w-full pt-56 pb-56 md:h-[80vh] md:pt-[235px]">
-        <Image
-          alt="tourImage"
-          src={imgSrc}
-          fill
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
-          priority
-          quality={60}
-          // placeholder="blur"
-          // blurDataURL={modifiedImageUrl}
-          className=" object-cover"
+      <div className="relative  w-full h-[224px]  md:h-[80vh]  ">
+        <video
+          className=" w-full h-full object-cover"
+          src={"https://api.nilecruisez.com/video/slidervideo.mp4"}
+          autoPlay
+          loop
+          muted
         />
         {/* layout */}
-        <div
-          className="absolute top-0 left-0 h-full w-full "
-          style={{
-            background:
-              "linear-gradient(270deg, rgba(5, 16, 54, 0.5) 0%, #051036 72.43%)",
-            opacity: 0.85,
-          }}
-        ></div>
-        {/* content */}
+        <div className="absolute top-0 left-0 w-full h-full bg-[##0000005e]  "></div>
       </div>
     </div>
   );
