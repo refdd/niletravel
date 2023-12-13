e21eea65a5c9e3769fa38ec2bceaa61e31ff70bf
mohamed refat <92251671+refdd@users.noreply.github.com>
2023-12-13 09:43:37 +0200

    remove video from header

--- src/components/headers/MainHeader.jsx
+++ src/components/headers/MainHeader.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import bg from "../../../public/assets/images/bg.webp";
 async function MainHeader() {
   return (
     <div className="">
-      <div className="relative  w-full h-[90vh]  md:h-[80vh]  ">
-        <video
-          className=" w-full h-full object-cover"
-          src={"https://api.nilecruisez.com/video/slidervideo.mp4"}
-          autoPlay
-          loop
-          muted
-        />
+      <div
+        style={{ backgroundImage: `url(${bg?.src})` }}
+        className="relative  w-full  pt-[120px] md:pt-[370px] pb-[480px] bg-center bg-no-repeat bg-cover  "
+      >
         {/* layout */}
-        <div className="absolute top-0 left-0 w-full h-full bg-[##0000005e] "></div>
+        <div
+          className="absolute top-0 left-0 h-[98%] w-full "
+          style={{
+            background:
+              "linear-gradient(270deg, rgba(5, 16, 54, 0.5) 0%, #4037b833 72.43%)",
+            opacity: 0.85,
+          }}
+        ></div>
       </div>
     </div>
   );
