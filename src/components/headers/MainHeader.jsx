2ac410325aabfa94aa6ed4dc240244565a87b888
mohamed refat <92251671+refdd@users.noreply.github.com>
2023-12-13 09:49:10 +0200

    use image tag

--- src/components/headers/MainHeader.jsx
+++ src/components/headers/MainHeader.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import bg from "../../../public/assets/images/bg.webp";
+import Image from "next/image";
 async function MainHeader() {
   return (
     <div className="">
-      <div
-        style={{ backgroundImage: `url(${bg?.src})` }}
-        className="relative  w-full  pt-[120px] md:pt-[370px] pb-[480px] bg-center bg-no-repeat bg-cover  "
-      >
+      <div className="relative  w-full pt-40 pb-48 md:h-[80vh] md:pt-[235px]">
+        <Image
+          alt="tourImage"
+          src={bg}
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
+          priority
+          quality={60}
+          // placeholder="blur"
+          // blurDataURL={modifiedImageUrl}
+          className=" object-cover"
+        />
         {/* layout */}
         <div
-          className="absolute top-0 left-0 h-[98%] w-full "
+          className="absolute top-0 left-0 h-full w-full "
           style={{
             background:
-              "linear-gradient(270deg, rgba(5, 16, 54, 0.5) 0%, #4037b833 72.43%)",
+              "linear-gradient(270deg, rgba(5, 16, 54, 0.5) 0%, #051036 72.43%)",
             opacity: 0.85,
           }}
         ></div>
+        {/* content */}
       </div>
     </div>
   );
