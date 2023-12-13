2f62f259bd89357d5f04189d84b339a842808903
mohamed refat <92251671+refdd@users.noreply.github.com>
2023-12-13 10:14:12 +0200

    add slider one

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
-import HeaderSections from "@/components/headers/HeaderSections";
+import MainHeader from "@/components/headers/MainHeader";
 import dynamic from "next/dynamic";
 import React, { Suspense } from "react";
-import { getData } from "../../utils/featchApi";
 // import RowsPackage from "@/components/tours/RowsPackage";
 // import RowsOffers from "@/components/offers/RowsOffers";
 // import FindPerfectTour from "@/components/perfectTour/FindPerfectTour";
@@ -17,8 +16,7 @@ const ExploreSection = dynamic(() =>
 const OurPartners = dynamic(() => import("@/components/partners/OurPartners"));
 const FaqSection = dynamic(() => import("@/components/FaqSection/FaqSection"));
 // const RowBlog = dynamic(() => import("@/components/blog/RowBlog"));
-const ToursSlider = dynamic(() => import("@/components/tours/ToursSlider"));
-const MainHeader = dynamic(() => import("@/components/headers/MainHeader"));
+const RowsPackage = dynamic(() => import("@/components/tours/RowsPackage"));
 // const RowsOffers = dynamic(() => import("@/components/offers/RowsOffers"));
 const Testimonials = dynamic(() =>
   import("@/components/testimonials/Testimonials")
@@ -26,24 +24,13 @@ const Testimonials = dynamic(() =>
 const FindPerfectTour = dynamic(() =>
   import("@/components/perfectTour/FindPerfectTour")
 );
-async function HomePage() {
-  const tours = await getData("/tours?limit=7");
+function HomePage() {
   return (
     <div>
       <MainHeader />
       <Suspense fallback={<>-----------</>}>
         {/* <SearchSection /> */}
-        <div className="pt-20">
-          <HeaderSections
-            title={""}
-            decs={
-              "Best-Selected Egypt Tour Packages. Choose from wide range of Egypt Travel Packages & get the chance to experience all that Egypt has to offer. Book Today!"
-            }
-            partOne={"Egypt Tour "}
-            partTwo={"Packages"}
-          />
-          <ToursSlider toursData={tours?.data} slugType={""} />
-        </div>
+        <RowsPackage />
         {/* <RowsOffers /> */}
         <FindPerfectTour />
         <Testimonials />
