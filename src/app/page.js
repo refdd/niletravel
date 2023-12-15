d639e4e819bc5ff0970d797951c324657dfb159a
mohamed refat <92251671+refdd@users.noreply.github.com>
2023-12-15 03:18:37 +0200

    test3

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,23 @@
 import MainHeader from "@/components/headers/MainHeader";
 import dynamic from "next/dynamic";
 import React, { Suspense } from "react";
+// import RowsPackage from "@/components/tours/RowsPackage";
+// import RowsOffers from "@/components/offers/RowsOffers";
+// import FindPerfectTour from "@/components/perfectTour/FindPerfectTour";
+// import Testimonials from "@/components/testimonials/Testimonials";
+// import RowBlog from "@/components/blog/RowBlog";
+// import ExploreSection from "@/components/explore/ExploreSection";
+// import OurPartners from "@/components/partners/OurPartners";
+// import FaqSection from "@/components/FaqSection/FaqSection";
+// const RowBlog = dynamic(() => import("@/components/blog/RowBlog"));
 const ExploreSection = dynamic(() =>
   import("@/components/explore/ExploreSection")
 );
 const OurPartners = dynamic(() => import("@/components/partners/OurPartners"));
 const FaqSection = dynamic(() => import("@/components/FaqSection/FaqSection"));
+// const RowBlog = dynamic(() => import("@/components/blog/RowBlog"));
 const RowsPackage = dynamic(() => import("@/components/tours/RowsPackage"));
+// const RowsOffers = dynamic(() => import("@/components/offers/RowsOffers"));
 const Testimonials = dynamic(() =>
   import("@/components/testimonials/Testimonials")
 );
@@ -18,9 +29,13 @@ function HomePage() {
     <div>
       <MainHeader />
       <Suspense fallback={<>-----------</>}>
+        {/* <SearchSection /> */}
         <RowsPackage />
+        {/* <RowsOffers /> */}
         <FindPerfectTour />
         <Testimonials />
+        {/* <RowBlog /> */}
+
         <ExploreSection />
         <OurPartners />
         <FaqSection />
